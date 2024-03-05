1da513dfa5c7a313dec85007a4a3c651fa9d497c
Your Name <sahilsanklas2736@gmail.com>
2024-03-05 12:58:33 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,4 @@
-
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,4 +29,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    
+    logger.LogError(ex,"An error occured during Migration");
+}
 app.Run();
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using Core.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        
+    }
+}
