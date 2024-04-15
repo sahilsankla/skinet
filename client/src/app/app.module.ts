846d4c1e0c5357af96211b75c0eebfc145f622c6
Your Name <sahilsanklas2736@gmail.com>
2024-04-15 12:54:53 +0530

    End of Section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,22 +3,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
    BrowserAnimationsModule,
-   HttpClientModule
-    
+   HttpClientModule,
+   CoreModule,
+   ShopModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
